9a669edb239ad182ee9aa9956a6d55f64f3755f5
officialwork-design <a.officialwork217@gmail.com>
2026-07-01 17:32:03 +0900

    Add DietManager config template

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DIET_MANAGER_CONFIG = {
+  APP_NAME: "DietManager",
+  LIFF_ID: "入力待ち",
+  GAS_WEB_APP_URL: "入力待ち",
+  GITHUB_PAGES_URL: "https://officialwork-design.github.io/DietManager/"
+};
