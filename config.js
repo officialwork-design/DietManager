ccc927699d574592de461191091cccbdbdd6f0a0
officialwork-design <a.officialwork217@gmail.com>
2026-07-01 18:46:30 +0900

    Update DietManager config values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DIET_MANAGER_CONFIG = {
   APP_NAME: "DietManager",
-  LIFF_ID: "入力待ち",
-  GAS_WEB_APP_URL: "入力待ち",
+  LIFF_ID: "2010567674-H2iadpu4",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycby6v4qJsgjf8gSrjQGabbhA56Uk4CU8QX59LZBjphlkCZVmE7TUiobBL4pyw83HgjTk/exec",
   GITHUB_PAGES_URL: "https://officialwork-design.github.io/DietManager/"
 };
